13e511fe307824fd4d63b9daaab1f3a6fb6d787c
Kashihara <KengoKashihara1998@gmail.com>
2020-03-17 11:49:43 +0900

    カードをランダムで配置
    ５２枚のカードを4×13でランダムに配置しました

--- Concentrate/Concentrate.cpp
+++ Concentrate/Concentrate.cpp
@@ -1,6 +1,19 @@
 #include"Concentrate.h"
+#include"../Trump.h"
 
 void ConcentrateMain()
 {
+	Trump trump[SUIT_MAX][NUMBER_MAX];
+	int i, j;
+
+	// ƒgƒ‰ƒ“ƒv‚ÌŽæ“¾
+	for (i = 0; i < SUIT_MAX; i++)
+	{
+		for (j = 0; j < NUMBER_MAX; j++)
+		{
+			trump[i][j] = GetTrump();
+			printf("%s‚Ì%d\n", trump[i][j].suit, trump[i][j].number);
+		}
+	}
 
 }
