a35bc4f2c7913807696276c7de4c23b00d738436
Kashihara <KengoKashihara1998@gmail.com>
2020-03-17 12:35:36 +0900

    入力されたカードを表示
    A～M、１～４で入力された文字を変換してカードを表示しました

--- Concentrate/Concentrate.cpp
+++ Concentrate/Concentrate.cpp
@@ -1,9 +1,16 @@
 #include"Concentrate.h"
 #include"../Trump.h"
 
+#include<string.h>
+
+// •¶š‚ğ”š‚É•ÏŠ·
+int ConvertInt(char c);
+
 void ConcentrateMain()
 {
 	Trump trump[SUIT_MAX][NUMBER_MAX];
+	char input[256];
+	int row, column;
 	int i, j;
 
 	// ƒgƒ‰ƒ“ƒv‚Ìæ“¾
@@ -12,8 +19,46 @@ void ConcentrateMain()
 		for (j = 0; j < NUMBER_MAX; j++)
 		{
 			trump[i][j] = GetTrump();
-			printf("%s‚Ì%d\n", trump[i][j].suit, trump[i][j].number);
+			//printf("%s‚Ì%d\n", trump[i][j].suit, trump[i][j].number);
 		}
 	}
 
+	// “ü—Í1
+	scanf("%s", input);
+	row = ConvertInt(input[0]);
+	column = ConvertInt(input[1]);
+	printf("%s‚Ì%d\n", trump[row][column].suit, trump[row][column].number);
+
+	// “ü—Í‚Q
+	scanf("%s", input);
+	row = ConvertInt(input[0]);
+	column = ConvertInt(input[1]);
+	printf("%s‚Ì%d\n", trump[row][column].suit, trump[row][column].number);
+}
+
+// •¶š‚ğ”š‚É•ÏŠ·
+int ConvertInt(char c)
+{
+	int convert = NUMBER_MAX;
+	char alphabet[] = 
+	{
+		'A','B','C','D','E','F','G','H','I','J','K','L','M'
+	};
+	char number[] =
+	{
+		'1','2','3','4'
+	};
+
+	char* p = strchr(alphabet, c);
+	if (p != NULL)
+	{
+		convert = p - alphabet;
+	}
+	else
+	{
+		p = strchr(number, c);
+		convert = p - number;
+	}
+
+	return convert;
 }
